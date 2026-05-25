fa863e8757397a604a1fc8becfb49f663a09bd59
KristianRadkov <radkovkristian@gmail.com>
2026-05-25 17:36:59 +0300

    Add genesis functionality.

--- examples/MySettlement.sol
+++ examples/MySettlement.sol
@@ -12,14 +12,14 @@ contract MySettlement is Settlement, OzAccessControl {
     bytes32 public constant SET_SIG_VERIFIER_ROLE = keccak256("SET_SIG_VERIFIER_ROLE");
     bytes32 public constant SET_GENESIS_ROLE = keccak256("SET_GENESIS_ROLE");
 
-    function initialize(SettlementInitParams memory settlementInitParams, address defaultAdmin)
-        public
-        virtual
-        initializer
-    {
+    function initialize(
+        SettlementInitParams memory settlementInitParams,
+        address defaultAdmin
+    ) public virtual initializer {
         __Settlement_init(settlementInitParams);
 
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(SET_GENESIS_ROLE, defaultAdmin);
 
         _setSelectorRole(ISettlement.setSigVerifier.selector, SET_SIG_VERIFIER_ROLE);
         _setSelectorRole(ISettlement.setGenesis.selector, SET_GENESIS_ROLE);
